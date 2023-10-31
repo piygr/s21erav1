fcc18c12b689db2fe63e09753032e76c25959b10
piygr <grover1piyush@gmail.com>
2023-10-31 20:42:59 +0530

    added model files

---
+++ data/cricket_commentary/prepare.py
@@ -0,0 +1,42 @@
+import csv
+import os
+import tiktoken
+import numpy as np
+
+input_file_path = os.path.join(os.path.dirname(__file__), 'commentary.txt')
+if not os.path.exists(input_file_path):
+    with open('IPL_Match_Highlights_Commentary.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                #print(row)
+                pass
+            else:
+                with open(input_file_path, 'w') as f:
+                    f.write( '%s\n%s overs\n%s\n' % (row[1].strip(), row[2].strip(), row[3].strip()) )
+
+            line_count += 1
+
+        print('Processed %s lines.' % line_count)
+
+
+with open(input_file_path, 'r') as f:
+    data = f.read()
+n = len(data)
+train_data = data[:int(n*0.9)]
+val_data = data[int(n*0.9):]
+
+# encode with tiktoken gpt2 bpe
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
+
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
